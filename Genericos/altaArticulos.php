ca39ebad1a50433d9122da141056de275fc2ddd9
AnaPatriciaL <paty.lopezvalenzuela@gmail.com>
2016-02-23 10:28:01 -0700

    Alta

--- Genericos/altaArticulos.php
+++ Genericos/altaArticulos.php
@@ -35,9 +35,9 @@
 					<label class="active" for="txtMarcaArt">Marca</label>
 				</div>
 				<div class="input-field col s2 offset-s1">
-					<input id="txtFechaCaducidad" type="text" class="validate">
-					<label class="active" for="txtFechaCaducidad">Fecha de caducidad</label>
-				</div>
+					<input id="txtTipoContenedor" type="text" class="validate">
+					<label class="active" for="txtTipoContenedor">Tipo de contenedor</label>
+				</div>		
 			</div>
 			<div class="row">
 				<div class="input-field col s6">
@@ -54,11 +54,11 @@
 					<input id="txtUnidadMedida" type="text" class="validate">
 					<label class="active" for="txtUnidadMedida">Unidad de medida</label>
 				</div>
-				<div class="input-field col s2 offset-s1">
-					<input id="txtTipoContenedor" type="text" class="validate">
-					<label class="active" for="txtTipoContenedor">Tipo de contenedor</label>
-				</div>
-				<div class="col s5 offset-s2">
+				<div class="input-field col s3  offset-s1">
+					<input id="txtFechaCaducidad" type="text" class="validate">
+					<label class="active" for="txtFechaCaducidad">Caducidad(dd/mm/aaaa)</label>
+				</div>	
+				<div class="col s5 offset-s1">
 					<a id="btnAltaArt" class="waves-effect waves-light btn green darken-2"><i class="material-icons left">done</i>Dar de alta</a>
 					<a id="btnCancelarAlta" class="waves-effect red darken-1 btn"><i class="material-icons left">clear</i>Cancelar</a>
 				</div>
