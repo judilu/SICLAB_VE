159acb074acb1484e52abc029d43155e0ab9ce1b
unknown <edwinfernando22@gmail.com>
2016-04-11 01:52:57 -0600

    Consulta datos del alumno con numero de control

--- js/alumnos.js
+++ js/alumnos.js
@@ -14,42 +14,94 @@ var inicio = function()
 	var uno= function()
 	{
 			$("#txtNControl").val($("#txtNControl").val()+1);
+			str=$("#txtNControl").val();
+			if(str.length==8)
+			{
+				consultaAlumno(str);
+			}
+			
 	}
 	var dos= function()
 	{
 			$("#txtNControl").val($("#txtNControl").val()+2);
+			str=$("#txtNControl").val();
+			if(str.length==8)
+			{
+				consultaAlumno(str);
+			}
 	}
 	var tres= function()
 	{
 			$("#txtNControl").val($("#txtNControl").val()+3);
+			str=$("#txtNControl").val();
+			if(str.length==8)
+			{
+				consultaAlumno(str);
+			}
 	}
 	var cuatro= function()
 	{
 			$("#txtNControl").val($("#txtNControl").val()+4);
+			str=$("#txtNControl").val();
+			if(str.length==8)
+			{
+				consultaAlumno(str);
+			}
 	}
 	var cinco= function()
 	{
 			$("#txtNControl").val($("#txtNControl").val()+5);
+			str=$("#txtNControl").val();
+			if(str.length==8)
+			{
+				consultaAlumno(str);
+			}
 	}
 	var seis= function()
 	{
 			$("#txtNControl").val($("#txtNControl").val()+6);
+			str=$("#txtNControl").val();
+			if(str.length==8)
+			{
+				consultaAlumno(str);
+			}
 	}
 	var siete= function()
 	{
 			$("#txtNControl").val($("#txtNControl").val()+7);
+			str=$("#txtNControl").val();
+			if(str.length==8)
+			{
+				consultaAlumno(str);
+			}
 	}
 	var ocho= function()
 	{
 			$("#txtNControl").val($("#txtNControl").val()+8);
+			str=$("#txtNControl").val();
+			if(str.length==8)
+			{
+				consultaAlumno(str);
+			}
 	}
 	var nueve= function()
 	{
 			$("#txtNControl").val($("#txtNControl").val()+9);
+			str=$("#txtNControl").val();
+			if(str.length==8)
+			{
+				consultaAlumno(str);
+			}
 	}
 	var cero= function()
 	{
 			$("#txtNControl").val($("#txtNControl").val()+0);
+			str=$("#txtNControl").val();
+			if(str.length==8)
+			{
+				consultaAlumno(str);
+
+			}
 	}
 	var ma= function()
 	{
@@ -71,6 +123,71 @@ var inicio = function()
 				objEntrada.value = objEntrada.value.substring(objEntrada.value.length);
 		}
 	}
+
+	//FUNCIONES PHP
+	var consultaAlumno = function(str)
+	{
+		var parametros = "opc=consultaAlumno"+
+    			"&nControl="+str+
+    			"&id="+Math.random();
+    			$.ajax({
+    				cache:false,
+    				type: "POST",
+    				dataType: "json",
+    				url:"../data/alumnos.php",
+    				data: parametros,
+    				success: function(response)
+    				{
+    					if(response.respuesta)
+    					{
+   							document.getElementById('txtNombre').focus()
+    						$("#txtNombre").val(response.ALUAPP+" "+ response.ALUAPM+ response.ALUNOM);	
+    						consultaCarrera(str);
+    					}
+    					else
+    					{
+    						sweetAlert("NO ENCONTRADO", "", "error");
+    					}
+    				},
+    				error: function(xhr, ajaxOptions,x)
+    				{
+    					console.log("Error de conexión");
+    				}
+    			});
+	}
+
+	var consultaCarrera = function(str)
+	{
+		var parametros = "opc=consultaCarrera"+
+    			"&nControl="+str+
+    			"&id="+Math.random();
+    			$.ajax({
+    				cache:false,
+    				type: "POST",
+    				dataType: "json",
+    				url:"../data/alumnos.php",
+    				data: parametros,
+    				success: function(response)
+    				{
+    					if(response.respuesta)
+    					{
+   							document.getElementById('txtCarrera').focus();
+    						$("#txtCarrera").val(response.CARNOM);
+    						document.getElementById('txtSemestre').focus();	
+    						$("#txtSemestre").val(response.CALNPE);
+    					}
+    					else
+    					{
+    						sweetAlert("NO ENCONTRADO", "", "error");
+    					}
+    				},
+    				error: function(xhr, ajaxOptions,x)
+    				{
+    					console.log("Error de conexión");
+    				}
+    			});
+	}
+
 	$("#btnPractica").on("click",practicaAlumnos);
 	$("#btnMaterialAlumno").on("click",materialPractica);
 
